3fbc201d8331713fbe6431d918079bf48702c26f
unknown <dennisworksglobal@gmail.com>
2021-03-01 00:47:55 +0200

    first commit

---
+++ SourceCode/Runtime/GameFramework/GameCharCodeMap.cpp
@@ -0,0 +1,132 @@
+#include "GameCharCodeMap.h"
+
+const TArray<FCharCodeMap> GreekCharCodeMap =
+{
+    { 'A', 'A' },
+    { 'B', 'B' },
+    { 'C', 'Y' },
+    { 'D', 'D' },
+    { 'E', 'E' },
+    { 'F', 'F' },
+    { 'G', 'G' },
+    { 'H', 'H' },
+    { 'I', 'I' },
+    { 'J', 'X' },
+    { 'K', 'K' },
+    { 'L', 'L' },
+    { 'M', 'M' },
+    { 'N', 'N' },
+    { 'O', 'O' },
+    { 'P', 'P' },
+    { 'U', 'Q' },
+    { 'R', 'R' },
+    { 'S', 'S' },
+    { 'T', 'T' },
+    { 'V', 'W' },
+    { 'X', 'C' },
+    { 'Y', 'U' },
+    { 'Z', 'Z' },
+    { 'Q', 'Q' },
+    { 'W', 'W' },
+    { '0', '0' },
+    { '1', '1' },
+    { '2', '2' },
+    { '3', '3' },
+    { '4', '4' },
+    { '5', '5' },
+    { '6', '6' },
+    { '7', '7' },
+    { '8', '8' },
+    { '9', '9' },
+    { 'a', 'a' },
+    { 'b', 'b' },
+    { 'c', 'y' },
+    { 'd', 'd' },
+    { 'e', 'e' },
+    { 'f', 'f' },
+    { 'g', 'g' },
+    { 'h', 'h' },
+    { 'i', 'i' },
+    { 'j', 'x' },
+    { 'k', 'k' },
+    { 'l', 'l' },
+    { 'm', 'm' },
+    { 'n', 'n' },
+    { 'o', 'o' },
+    { 'p', 'p' },
+    { 'u', 'q' },
+    { 'r', 'r' },
+    { 's', 's' },
+    { 't', 't' },
+    { 'v', 'w' },
+    { 'x', 'c' },
+    { 'y', 'u' },
+    { 'z', 'z' },
+    { 'q', 'q' },
+    { 'w', 'w' },
+
+};
+
+static const TArray<TCHAR> GreekCharCodeExceptions =
+{
+    'Q', 'W',
+};
+
+static FGameCharCodeMap FGameCharCodeMapGlobal;
+
+FGameCharCodeMap *
+FGameCharCodeMap::Get()
+{
+    return &FGameCharCodeMapGlobal;
+}
+
+void
+FGameCharCodeMap::SetGameLanguage(EGameLanguage InGameLanguage)
+{
+    GameLanguage = InGameLanguage;
+}
+
+bool 
+FGameCharCodeMap::IsCharCodePresent(const FCharCodeMap &CharCodeMap)
+{
+    switch(GameLanguage)
+    {
+    case GL_English:
+        return CharCodeMap.CharCode;
+        break;
+    case GL_Greek:
+        return !GreekCharCodeExceptions.Contains(CharCodeMap.KeyCode);
+        break;
+    }
+
+    return false;
+}
+
+TCHAR 
+FGameCharCodeMap::GetMappedCharCode(const FCharCodeMap &CharCodeMap)
+{   
+    switch(GameLanguage)
+    {
+    case GL_English:
+        return CharCodeMap.KeyCode;
+        break;
+    case GL_Greek:
+        return CharCodeMap.CharCode;
+        break;
+    }
+
+    return 0;
+}
+
+TCHAR
+FGameCharCodeMap::GetMappedCharCodes(const TCHAR& Letter)
+{   
+    for (uint32 Index = 0; Index < GreekCharCodeMap.GetCapacity(); Index++)
+    {
+        if (GreekCharCodeMap[Index].KeyCode == Letter)
+        {
+            return GreekCharCodeMap[Index].CharCode;
+        }
+    }
+    return 0;
+}
